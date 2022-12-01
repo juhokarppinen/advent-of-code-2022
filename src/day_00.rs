ba35b5f63975b94243a222d2bc5a51357cabe27c
Juho Karppinen <juho@juhokarppinen.fi>
2022-12-01 13:09:15 +0200

    extract solving logic outside module entry point

--- src/day_00.rs
+++ src/day_00.rs
@@ -1,14 +1,15 @@
 use std::fs;
 use std::io;
 
-const DAY: &'static str = "01";
+const DAY: u8 = 0;
+
+pub fn get_solution() -> io::Result<(u8, i32, i32)> {
+    let input = fs::read_to_string(format!("./input/{DAY:0>2}.txt"))?;
+    let (part_1, part_2) = solve(&input);
+    Ok((DAY, part_1, part_2))
+}
 
 /// https://adventofcode.com/2022/day/0
-pub fn get_solution() -> io::Result<(String, i32, i32)> {
-    let input = fs::read_to_string(format!("./input/{DAY}.txt"))?;
-
-    let part_1 = 0;
-    let part_2 = 0;
-
-    Ok((format!("Day {DAY}"), part_1, part_2))
+fn solve(input: &str) -> (i32, i32) {
+    (0, 0)
 }
--- src/day_01.rs
+++ src/day_01.rs
@@ -1,25 +1,24 @@
 use std::fs;
 use std::io;
 
-const DAY: &'static str = "01";
+const DAY: u8 = 1;
+
+pub fn get_solution() -> io::Result<(u8, i32, i32)> {
+    let input = fs::read_to_string(format!("./input/{DAY:0>2}.txt"))?;
+    let (part_1, part_2) = solve(&input);
+    Ok((DAY, part_1, part_2))
+}
 
 /// https://adventofcode.com/2022/day/1
-pub fn get_solution() -> io::Result<(String, i32, i32)> {
-    let input = fs::read_to_string(format!("./input/{DAY}.txt"))?;
-
-    let mut calories_per_elf: Vec<i32> = input
+fn solve(input: &str) -> (i32, i32) {
+    let mut cals_per_elf: Vec<i32> = input
         .split("\n\n")
         .map(|c| c.lines().map(|line| line.parse().unwrap_or(0)).sum())
         .collect();
-    calories_per_elf.sort_unstable();
-    calories_per_elf.reverse();
-
-    let part_1 = sum(1, &calories_per_elf);
-    let part_2 = sum(3, &calories_per_elf);
-
-    Ok((format!("Day {DAY}"), part_1, part_2))
-}
-
-fn sum(n: usize, sums: &Vec<i32>) -> i32 {
-    sums.iter().take(n).sum()
+    cals_per_elf.sort_unstable();
+    cals_per_elf.reverse();
+    (
+        cals_per_elf.iter().take(1).sum(),
+        cals_per_elf.iter().take(3).sum(),
+    )
 }
